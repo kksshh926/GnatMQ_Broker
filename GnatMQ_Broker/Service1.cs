7ae1ded163d9a147681bb12d6bc92f5bd9ffb5ab
kksshh926 <kksshh926@naver.com>
2022-10-26 22:16:42 +0900

    초기 커밋.

---
+++ GnatMQ_Broker/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace GnatMQ_Broker
+{
+    public partial class Service1 : ServiceBase
+    {
+        MqttBroker broker = new MqttBroker();
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            broker.Start();
+            
+        }
+
+        protected override void OnStop()
+        {
+            broker.Stop();
+        }
+    }
+}
